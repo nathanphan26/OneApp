b5ff468c5550f46a42e17b6ead35e5ce49ee1112
Nathan Phan <nathphan510@gmail.com>
2017-10-17 20:33:03 -0700

    Database work

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 // Register
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
 	res.send('REGISTER');
 }); 
 
-// Register
+// Authenticate
 router.post('/authenticate', (req, res, next) => {
 	res.send('authenticate');
 }); 
 
-// Register
+// Profile
 router.get('/profile', (req, res, next) => {
 	res.send('profile');
-}); 
-
-// Register
-router.get('/validate', (req, res, next) => {
-	res.send('VALIDATE');
 }); 
 
 module.exports = router;
