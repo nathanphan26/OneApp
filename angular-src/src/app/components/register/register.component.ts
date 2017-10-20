a19237b0e5e3f5f29abae6db190bc8532f5cd679
Manav Sharma <msharma7@ucsc.edu>
2017-10-19 21:17:40 -0700

    Added more frontend stuff along with register page and validation

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service'
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  fname: String;
+  lname: String;
+  email: String;
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService, private flashMessage:FlashMessagesService) { }
 
   ngOnInit() {
   }
 
+  onRegisterSubmit(){
+    const user = {
+      fname: this.fname,
+      lname: this.lname,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    //Required fields
+    if(!this.validateService.validateRegister(user)){
+      this.flashMessage.show('Please fill in all the fields.', {cssClass: 'alert-danger', timeout: 4000});
+      return false;
+    }
+
+    //Validate Email
+    if(!this.validateService.validateEmail(user.email)){
+      this.flashMessage.show('Please enter a valid email.', {cssClass: 'alert-danger', timeout: 4000});
+      return false;
+  }
+}
 }
