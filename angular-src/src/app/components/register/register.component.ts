750501c0c6219031025408889a94ad40e00b74ed
Manav Sharma <msharma7@ucsc.edu>
2017-10-20 12:37:59 -0700

    Connected register form to the back end

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ValidateService} from '../../services/validate.service'
+import {AuthService} from '../../services/auth.service'
 import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +16,12 @@ export class RegisterComponent implements OnInit {
   username: String;
   password: String;
 
-  constructor(private validateService: ValidateService, private flashMessage:FlashMessagesService) { }
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage:FlashMessagesService,
+    private authService:AuthService,
+    private router: Router
+   ) { }
 
   ngOnInit() {
   }
@@ -39,5 +46,16 @@ export class RegisterComponent implements OnInit {
       this.flashMessage.show('Please enter a valid email.', {cssClass: 'alert-danger', timeout: 4000});
       return false;
   }
+
+  //Register User
+  this.authService.registerUser(user).subscribe(data => {
+    if (data.success){
+      this.flashMessage.show('You have registered successfully!', {cssClass: 'alert-success', timeout: 8000});
+      this.router.navigate(['/login']);
+    } else {
+      this.flashMessage.show('Something went wrong..', {cssClass: 'alert-danger', timeout: 8000});
+      this.router.navigate(['/register']);
+    }
+  });
 }
 }
