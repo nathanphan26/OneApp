ead67bc6beb8ec7447e5b42a117cd42884c8c57e
Nat <nathphan510@gmail.com>
2017-11-13 20:39:36 -0800

    added twitter api to grab timeline

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const Twitter = require('twitter'); //cs115julig
 
 mongoose.connect(config.url);
 
@@ -22,6 +23,26 @@ const users = require('./routes/users');
 
 // Port Number
 const port = 8000
+
+// Twitter Middleware
+var client = new Twitter({
+  consumer_key: 'syNTvTmQC6tnWnUnvY5NDNDPS',
+  consumer_secret: 'twBTVDXD2JicUMSbnjPlrblC53TZCmRO9Iyuo5izQ4vsL7HmwQ',
+  access_token_key: '916925589855932416-Trr3uaq8ZxuhjdQL5VeJZhhwAi7pDFc',
+  access_token_secret: 'bWib1xhYZIC197AFPg8sqCgSM7iyWCV3aKjkIX27SaoE3'
+});
+ 
+var params = {screen_name: 'cs115oneapp'};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    // console.log(tweets);
+    for (var key in tweets) {
+    	console.log(tweets[key].text);
+    }
+  }
+});
+
 
 // CORS Middleware
 app.use(cors());
